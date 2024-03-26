1ab5c707b5e580f6c89a5c0e0a75e242a220edf3
Alexander Linse <zandervon24@gmail.com>
2024-03-25 19:36:19 -0500

    Added puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
       <h1>Puppy Pals</h1>
+      { puppies.map(puppy => <p key={puppy.id}>{puppy.name}</p>) }
     </>
   )
 }
