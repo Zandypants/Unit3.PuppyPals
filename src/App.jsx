92fb38362e930cc032cba661a92b86ebae1d2273
Alexander Linse <zandervon24@gmail.com>
2024-03-25 20:12:24 -0500

    Added puppy selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log(puppies);
+
+  const featuredPup = puppies.find(puppy => puppy.id === featPupId);
+
   return (
     <>
       <h1>Puppy Pals</h1>
-      { puppies.map(puppy => <p key={puppy.id}>{puppy.name}</p>) }
+      { puppies.map(puppy => <p onClick={()=>setFeatPupId(puppy.id)} key={puppy.id}> {puppy.name} </p>) }
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   )
 }
